02563ea383ad8b7c69116180bb478d5ab97b02dd
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-05-18 17:49:45 +0530

    added resources
    added resources

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ weatherApp.config(function ($routeProvider) {
 
 // SERVICE
 weatherApp.service('cityService',function(){
-	this.city = "New York, NY";
+	this.city = "London,us";
 });
 
 
@@ -34,5 +34,8 @@ weatherApp.controller('homeController', ['$scope','cityService', function($scope
 
 weatherApp.controller('forecastController', ['$scope','$resource','cityService', function($scope,$resource,cityService) {
     $scope.city = cityService.city;
-	$scope.weatherAPI = $resource('http://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=b6907d289e10d714a6e88b30761fae22');
+	//http://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=b6907d289e10d714a6e88b30761fae22
+	$scope.weatherAPI = $resource('http://samples.openweathermap.org/data/2.5/forecast',{callback: "JSON_CALLBACK"},{get:{method:"JSONP"}});
+	$scope.weatherResult = $scope.weatherAPI.get({ q:$scope.city, appid : 'b6907d289e10d714a6e88b30761fae22' });
+	console.log($scope.weatherResult);
 }]);
